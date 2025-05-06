41f01bbfef516571f2031598d54396e5aee8b2ae
bpavuk <75901693+bpavuk@users.noreply.github.com>
2025-05-06 21:48:50 +0300

    added DAP UI

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -3,8 +3,8 @@ require "nvchad.mappings"
 -- add yours here
 
 local map = vim.keymap.set
-local tmux = require("tmux")
-local dap = require("dap")
+local tmux = require "tmux"
+local dap = require "dap"
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
@@ -12,18 +12,33 @@ map("n", "<leader>rs", function()
   vim.lsp.buf.rename()
 end, { desc = "Rename the symbol at cursor" })
 
-
 -- tmux navigation
-map("n", "<C-h>", function() tmux.move_left() end)
-map("n", "<C-j>", function() tmux.move_bottom() end)
-map("n", "<C-k>", function() tmux.move_top() end)
-map("n", "<C-l>", function() tmux.move_right() end)
-
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map("n", "<C-h>", function()
+  tmux.move_left()
+end)
+map("n", "<C-j>", function()
+  tmux.move_bottom()
+end)
+map("n", "<C-k>", function()
+  tmux.move_top()
+end)
+map("n", "<C-l>", function()
+  tmux.move_right()
+end)
 
 -- DAP
-map("n", "<leader>dba", function() dap.toggle_breakpoint() end, { desc = "Toggle breakpoint" })
-map("n", "<leader>dbc", function() dap.continue() end, { desc = "Continue debugging" })
-map("n", "<leader>dbso", function() dap.step_over() end, { desc = "Step over" })
-map("n", "<leader>dbsi", function() dap.step_into() end, { desc = "Step into" })
-
+map("n", "<leader>dba", function()
+  dap.toggle_breakpoint()
+end, { desc = "Toggle breakpoint" })
+map("n", "<leader>dbc", function()
+  dap.continue()
+end, { desc = "Continue debugging" })
+map("n", "<leader>dbso", function()
+  dap.step_over()
+end, { desc = "Step over" })
+map("n", "<leader>dbsi", function()
+  dap.step_into()
+end, { desc = "Step into" })
+map("n", "<leader>dbsO", function()
+  dap.step_out()
+end, { desc = "Step out" })
--- nvim/.config/nvim/lua/plugins/dap.lua
+++ nvim/.config/nvim/lua/plugins/dap.lua
@@ -2,4 +2,14 @@ return {
   {
     "mfussenegger/nvim-dap",
   },
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "nvim-neotest/nvim-nio",
+    },
+    config = function()
+      require "configs.dap_ui"
+    end,
+  },
 }
